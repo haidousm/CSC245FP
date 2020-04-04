c938c27fba7210192f9da472571383c0083180e3
haidousm <haidous.m@gmail.com>
2020-04-04 17:46:04 +0300

    create radiologist class

---
+++ src/physician_component/Radiologist.java
@@ -0,0 +1,10 @@
+package physician_component;
+
+public class Radiologist extends Physician{
+
+    public Radiologist(int ID, String firstName, String lastName) {
+        super(ID, firstName, lastName);
+        
+    }
+
+}
