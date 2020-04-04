476bbde0c746ecbeb5dacec11e226b40236dd422
M3hdi <32139047+mehdimerbah@users.noreply.github.com>
2020-04-05 02:18:44 +0300

    Update PhysicalConsultationReport.java

--- src/lab_tech_component/reports/PhysicalConsultationReport.java
+++ src/lab_tech_component/reports/PhysicalConsultationReport.java
@@ -1,5 +1,34 @@
 package lab_tech_component.reports;
 
-public class PhysicalConsultationReport extends MedicalReport {
+import patient_component.Patient;
+import lab_tech_component.interfaces.IGenerateReport;
+
+
+public class PhysicalConsultationReport extends MedicalReport implements IGenerateReport {
+
+	public PhysicalConsultationReport(int report_ID, String type, Patient patient, int freqOfTreatment,
+			String dateOfLastVisit) {
+		super(report_ID, type, patient, freqOfTreatment, dateOfLastVisit);
+		
+	}
+	
+	
+	public void printReport() {
+		System.out.println(this.toString());
+	}
+	
+	
+
+	public String toString() {
+		return String.format("\nReport ID:" + getReport_ID() + "\nReport Type: " + getType()
+				 + "\nFrequency of Visit: " + getFreqOfTreatment()
+				+ "\nLast Visit: " + getDateOfLastVisit() + "\nPatient:\n" + getPatient().toString());
+	}
+
+
+	
+
+	
+	
 
 }
