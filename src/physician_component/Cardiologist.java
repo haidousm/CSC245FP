37a42b661a8ac35209f4fb135f544289e9c9e873
haidousm <haidous.m@gmail.com>
2020-04-04 17:45:09 +0300

    create Cardiologist class

---
+++ src/physician_component/Cardiologist.java
@@ -0,0 +1,10 @@
+package physician_component;
+
+public class Cardiologist extends Physician{
+
+    public Cardiologist(int ID, String firstName, String lastName) {
+        super(ID, firstName, lastName);
+        
+    }
+    
+}
