8d204a8836d530de0a7ce95bf067afcd81020f46
M3hdi <32139047+mehdimerbah@users.noreply.github.com>
2020-04-05 02:15:46 +0300

    Update Medication.java

--- src/lab_tech_component/Medication.java
+++ src/lab_tech_component/Medication.java
@@ -4,8 +4,17 @@ public class Medication {
 
     private int medicationID;
     private String medicationName;
+    private String adverseMedication;
+ 
 
-    public int getMedicationID() {
+	public Medication(int medicationID, String medicationName, String adverseMedication) {
+		super();
+		this.medicationID = medicationID;
+		this.medicationName = medicationName;
+		this.adverseMedication = adverseMedication;
+	}
+
+	public int getMedicationID() {
         return this.medicationID;
     }
 
@@ -21,6 +30,14 @@ public class Medication {
         this.medicationName = medicationName;
     }
 
+    public String getAdverseMedication() {
+		return adverseMedication;
+	}
+
+	public void setAdverseMedication(String adverseMedication) {
+		this.adverseMedication = adverseMedication;
+	}
+	
     public String toString() {
         
         return String.format("Medication ID: %d -- Medication Name: %s\n", this.getMedicationID(), this.getMedicationName());
