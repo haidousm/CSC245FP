e136d032020a1d20f47d0c80aa61c590f08954eb
haidousm <haidous.m@gmail.com>
2020-04-04 17:54:12 +0300

    clean up patient class

--- src/patient_component/Patient.java
+++ src/patient_component/Patient.java
@@ -5,24 +5,26 @@ import general_files.User;
 import lab_tech_component.Diseases;
 import lab_tech_component.Medication;
 
-public class Patient extends User{
-	
+public class Patient extends User {
+
 	private ArrayList<Diseases> diseasesList = new ArrayList<>();
 	private ArrayList<Medication> medicationsList = new ArrayList<>();
 	private int age;
-	private double weight , height;
 	
-	//Two different constructors depending on type of info available
+	private double weight, height;
+
+	// Two different constructors depending on type of info available
 	public Patient(int ID, String firstName, String lastName, int age, double w, double h) {
 
 		super(ID, firstName, lastName);
 		this.age = age;
 		this.weight = w;
 		this.height = h;
-	
+
 	}
 
-	public Patient(int ID, String firstName, String lastName, int age,double w, double h, ArrayList<Medication> meds, ArrayList<Diseases> d) {
+	public Patient(int ID, String firstName, String lastName, int age, double w, double h, ArrayList<Medication> meds,
+			ArrayList<Diseases> d) {
 		super(ID, firstName, lastName);
 		this.age = age;
 		this.weight = w;
@@ -30,54 +32,60 @@ public class Patient extends User{
 		this.diseasesList = d;
 		this.medicationsList = meds;
 	}
-	
-	// GETTERS & SETTERS
-	public ArrayList<Diseases> getdiseasesList() {
+
+	public ArrayList<Diseases> getDiseasesList() {
 		return diseasesList;
 	}
 
-	public void setdiseasesList(ArrayList<Diseases> diseasesList) {
+	public void setDiseasesList(ArrayList<Diseases> diseasesList) {
 		this.diseasesList = diseasesList;
 	}
 
-	public ArrayList<Medication> getmedicationsList() {
+	public ArrayList<Medication> getMedicationsList() {
 		return medicationsList;
 	}
 
-	public void setmedicationsList(ArrayList<Medication> medicationsList) {
+	public void setMedicationsList(ArrayList<Medication> medicationsList) {
 		this.medicationsList = medicationsList;
 	}
 
 	public int getAge() {
-        return this.age;
-    }
-    
-    public void setAge(int age) {
-        this.age = age;
-    }
-     
-   public String getALLMeds() {
-	   String s="";
-	   for (int i = 0; i < this.medicationsList.size(); i++) {
-		s+=medicationsList.toString()+"\n";
+		return this.age;
 	}
-	   return s;
-   }
-   
-   public String getALLDiseases() {
-	   String s="";
-	   for (int i = 0; i < this.diseasesList.size(); i++) {
-		s+=medicationsList.toString()+"\n";
+
+	public void setAge(int age) {
+		this.age = age;
 	}
-	   return s;
-   }
-    
+
+	public String getMeds() {
+
+		String string = "";
+		for (int i = 0; i < this.medicationsList.size(); i++) {
+
+			string += medicationsList.get(i).toString();
+
+		}
+
+		return string;
+	}
+
+	public String getDiseases() {
+		String string = "";
+
+		for (int i = 0; i < this.diseasesList.size(); i++) {
+
+			string += diseasesList.get(i).toString();
+
+		}
+
+		return string;
+	}
+
 	@Override
 	public String toString() {
-		return "Patient ID: " + super.getID() + "\nName: "+super.getFirstName()+" "+super.getLastName()+
-				"\n\tDiseases Contracted: "+this.getALLDiseases()
-				+"\n\tMedication Currently Being Taken: "+ this.getALLMeds();
+
+		return String.format("Patient ID: %d -- Name: %s, %s -- Diseases Contracted:\n%s\nMedications In Use:\n%s",
+				this.getID(), this.getLastName(), this.getFirstName(), this.getDiseases(), this.getMeds());
 	}
-	
 
 }
